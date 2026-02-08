e315e8b8d6d41b1b588178cd822d4a453290a353
Hariom Rathore <imhariomr@gmail.com>
2026-02-08 12:13:17 +0530

    sharable be

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import express from "express";
+import "dotenv/config";
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(cors());
+
+const httpServer = http.createServer(app);
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*"
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected:", socket.id);
+
+  socket.emit("id", socket.id);
+
+  socket.on("signal", ({ to, data }) => {
+    io.to(to).emit("signal", {
+      from: socket.id,
+      data
+    });
+  });
+
+  socket.on("connection established", ({ to }) => {
+    io.to(to).emit("connection established",{
+      from: socket.id,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected:", socket.id);
+  });
+});
+
+httpServer.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
